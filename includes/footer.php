877c0e059435ed1fa03baaf078b8d2ec18aa4d9d
adamsgilly <154255497+adamsgilly@users.noreply.github.com>
2024-04-25 20:03:01 +0100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,82 @@
+ <section class="footer-area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-lg-4">
+                    <div class="footer-widget">
+                        <div class="footer-title">
+                            <img src="asset/img/logo.png" alt="">
+                        </div>
+                        <p>Over 24 years experience and knowledge Golden thataka secured loan wе’rе hеrе to рrоvіdе уоu wіth fіnаnсіаl ѕоlutіоnѕ fоr аll уоur lеndіng needs. Whether you are lооkіng fоr any kind of loan, оur lending tеаm wіll explain lеndіng орtіоnѕ to уоu іn a ѕtrаіghtfоrwаrd mаnnеr.</p>
+                        <div class="mail">
+                            <span>Any Queries :</span>support@goldenthanakaloan.com
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-lg-2">
+                    <div class="footer-widget pages-widget">
+                        <div class="footer-title">
+                            <h4>Solutions</h4>
+                        </div>
+                        <ul>
+                            <li><a href="apply.php">Personal Loan</a></li>
+                            <li><a href="apply.php">Car Loan</a></li>
+                            <li><a href="apply.php">Home Loan</a></li>
+                            <li><a href="apply.php">Education Loan</a></li>
+                            <li><a href="apply.php">Business Loan </a></li>
+                            <li><a href="apply.php">Perday Loan </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-5 col-lg-3">
+                    <div class="footer-widget recent-post">
+                        <div class="footer-title">
+                            <h4>Recent Posts</h4>
+                        </div>
+                        <ul>
+                            <li><a href="#">This book is a treatise on theory
+of ethics very popular. <span class="date">January 18, 2018</span></a></li>
+                            <li><a href="#">Be cites of the word in classical
+literature to discove. <span class="date">Februay 10, 2018</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget">
+                        <div class="footer-title">
+                            <h4>Newsletter</h4>
+                        </div>
+                        <div class="form-widget">
+                            <p>Subscribe to our newsletter and get latest news related to loan,credit cards,perday loan,interates rate.</p>
+                            <form action="#" method="get">
+                                <div class="newsletter">
+                                    <label for="envalope" class="fa fa-envelope"></label>
+                                    <input id="envalope" type="email" name="email" placeholder="Email address">
+                                </div>
+                                <button type="submit" class="button btn btn-default btn-lg">Subscribe +</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="copy-right-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-left">
+                    <div class="copyright-text">
+                        <p><i class="fa fa-copyright"></i> 2018 Golden thataka secured loan. All Rights Reserved</p>
+                    </div>
+                </div>
+                <div class="col-md-6 text-right">
+                    <div class="footer-nave">
+                        <a href="#"><i class="fa fa-facebook-square"></i></a>
+                        <a href="#"><i class="fa fa-twitter-square"></i></a>
+                        <a href="#"><i class="fa fa-linkedin-square"></i></a>
+                        <a href="#"><i class="fa fa-pinterest-square"></i></a>
+                        <a href="#"><i class="fa fa-vimeo-square"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ includes/header.php
@@ -0,0 +1,96 @@
+<header class="header-area">
+        <div class="topbar-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-left">
+                        <div class="left-info">
+                            <p><span class="fa fa-clock"></span>Opening Hours : Mon - Sat : 8.30 to 16.00</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 text-right">
+                        <div class="right-info">
+                            <p><a href="contact.php"><span><i class="fa fa-download"></i>Contact Us </span></a> <a href="#"><span><i class="fa fa-futbol-o"></i>Support</span> </a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="logo-right-address">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="sitelogo">
+                            <a href="index.php"><img src="asset/img/logo.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="right-address">
+                            <div class="single-address">
+                                <div class="icon">
+                                    <img src="asset/img/call.png" alt="">
+                                </div>
+                                <div class="content">
+                                    <p>
+                                        <span>Call Us:</span> +1 651 505 0987
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="single-address">
+                                <div class="icon">
+                                    <img src="asset/img/envalope.png" alt="">
+                                </div>
+                                <div class="content">
+                                    <p>
+                                        <span>Email us: </span> support@goldenthanakaloan.com
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="single-address">
+                                <a href="apply.php" class="button btn btn-default btn-sm"><i class="fa fa-reply-all"></i> APPLY NOW </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="menu-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-11 col-lg-11">
+                        <div id="cssmenu">
+                            <ul>
+                                <li><a href="index.php">Home</a>
+                                    
+                                </li>
+                                <li><a href="about.php">About us </a></li>
+                                <li><a href="services.php">Service</a>
+                                   
+                                </li>
+                                <li><a href="#">Pages</a>
+                                    <ul>
+                                        <li> <a href="calculator.php">Calculator</a></li>
+                                        <li> <a href="gallery.php">Gallery</a></li>
+                                        <li><a href="team.php">Team</a></li>
+                                        
+                                    </ul>
+                                </li>
+                                <li><a href="apply.php">Apply Now </a>
+                                    
+                                </li>
+                                <li><a href="contact.php">Contact us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-1 text-right">
+                        <div class="searchbtn">
+                            <a href="#" class="fa fa-search"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="searchform">
+            <input type="text" name="s" placeholder="Search Here">
+            <button><i class="fa fa-times"></i></button>
+        </div>
+    </header>
